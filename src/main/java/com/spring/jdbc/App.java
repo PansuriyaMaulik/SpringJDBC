972ab818f48bb681d077560936e484598e894e4c
Maulik Pansuriya <maulikpansuriya36@gmail.com>
2022-11-24 23:41:18 +0100

    Established the database connection and insert the record into db

--- src/main/java/com/spring/jdbc/App.java
+++ src/main/java/com/spring/jdbc/App.java
@@ -1,5 +1,9 @@
 package com.spring.jdbc;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 /**
  * Hello world!
  *
@@ -8,7 +12,17 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println( "Program Started....!" );
 
-        System.out.println( "Hello World!" );
+        //Spring JDBC => JdbcTemplate
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        JdbcTemplate template = context.getBean("jdbcTemplate", JdbcTemplate.class);
+
+        //Insert data into database --- Query
+        String query = "insert into student(id,name,city) values(?,?,?)";
+
+        //Fire Query
+        int result = template.update(query, 4, "Ajay", "Nagpur");
+        System.out.println("Number of recorded inserted.." +result);
     }
 }
